31175fde077a022471abc2c3b0c4af6fb67f5719
hoon-hoon <kc0393@naver.com>
2025-03-04 15:42:09 +0900

    chore: Vite + React + TypeScript 환경 세팅

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div style={{ textAlign: "center", padding: "20px" }}>
+            <h1>올래스텝 프로젝트 🚀</h1>
+            <p>프론트엔드 기본 화면입니다.</p>
+        </div>
+    );
+}
+
+export default App;
