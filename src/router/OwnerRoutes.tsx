c0c4839a10afc1c5613c76bbb07b2819c9b33c7f
hoon-hoon <kc0393@naver.com>
2025-03-27 00:43:16 +0900

    feat: 역할 기반 초기 라우터 구조 세팅

---
+++ src/router/OwnerRoutes.tsx
@@ -0,0 +1,18 @@
+import { RouteObject } from "react-router-dom";
+import OwnerLayout from "../layout/OwnerLayout";
+import OwnerHome from "../pages/OwnerHome";
+
+const OwnerRoutes: RouteObject[] = [
+    {
+        path: "/owner",
+        element: <OwnerLayout />,
+        children: [
+            {
+                index: true,
+                element: <OwnerHome />,
+            },
+        ],
+    },
+];
+
+export default OwnerRoutes;
