20e4516fb7ef03d1ac373aae4d8d032d18d683d3
hoon-hoon <kc0393@naver.com>
2025-03-31 17:43:57 +0900

    feat: 카카오/네이버 소셜 로그인 api 연동 중

--- src/pages/auth/KakaoRedirectPage.tsx
+++ src/pages/auth/KakaoRedirectPage.tsx
@@ -1,37 +1,40 @@
 import { useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 // import useUserStore from "@/store/user";
 
 export default function KakaoRedirectPage() {
-    // const navigate = useNavigate();
+    const navigate = useNavigate();
     // const setUser = useUserStore((state) => state.setUser);
 
     useEffect(() => {
         const code = new URL(window.location.href).searchParams.get("code");
         console.log(code);
 
-        // if (code) {
-        //   axios
-        //     .post(`${import.meta.env.VITE_API_BASE_URL}/login/kakao`, { code })
-        //     .then((res) => {
-        //       const { status, data } = res.data;
-        //
-        //       if (status === "USER_NEED_SIGNUP") {
-        //         navigate("/signup");
-        //       }
-        //       // 회원 상태 저장 여기서
-        //       // setUser({
-        //       //   id: data.userId,
-        //       //   nickname: data.nickname,
-        //       //   type: data.type,
-        //       // });
-        //
-        //     })
-        //     .catch((err) => {
-        //       console.error("로그인 실패", err);
-        //     });
-        // }
+        if (code) {
+            axios
+                .get(`${import.meta.env.VITE_API_BASE_URL}/login/kakao/success?code=${code}`, {
+                    withCredentials: true,
+                })
+                .then((res) => {
+                    console.log(res);
+
+                    const { status, data } = res.data;
+
+                    if (status === "USER_NEED_SIGNUP") {
+                        navigate("/signup");
+                    }
+                    // 회원 상태 저장 여기서
+                    // setUser({
+                    //   id: data.userId,
+                    //   nickname: data.nickname,
+                    //   type: data.type,
+                    // });
+                })
+                .catch((err) => {
+                    console.error("로그인 실패", err);
+                });
+        }
     }, []);
 
     return <div>로그인 처리 중입니다...</div>;
