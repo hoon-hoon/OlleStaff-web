c3def5f59de158feb556b9194ceb15525a06e3df
ZUITOPIA <zuitopia.dev@gmail.com>
2025-04-14 21:38:44 +0900

    refactor: Menu 대신 Nav로 이름 통일

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,28 +3,28 @@ import { Wrapper } from "@/styles/Wrapper";
 import { useNavigate } from "react-router-dom";
 import styled from "@emotion/styled";
 
-type MenuProps = {
+type NavProps = {
     version: "owner" | "staff";
 };
 
-const menuItems = {
+const items = {
     owner: [
-        { src: "/MenuHome.svg", alt: "홈", label: "홈", path: "/owner" },
-        { src: "/MenuRecruit.svg", alt: "나의 공고", label: "나의 공고", path: "/my-notes" },
-        { src: "/MenuChat.svg", alt: "채팅", label: "채팅", path: "/chat" },
-        { src: "/MenuUser.svg", alt: "내 정보", label: "내 정보", path: "/ownerinfo" },
+        { src: "/NavHome.svg", alt: "홈", label: "홈", path: "/owner" },
+        { src: "/NavRecruit.svg", alt: "나의 공고", label: "나의 공고", path: "/my-notes" },
+        { src: "/NavChat.svg", alt: "채팅", label: "채팅", path: "/chat" },
+        { src: "/NavUser.svg", alt: "내 정보", label: "내 정보", path: "/ownerinfo" },
     ],
     staff: [
-        { src: "/MenuHome.svg", alt: "홈", label: "홈", path: "/staff" },
-        { src: "/MenuCompanion.svg", alt: "동행", label: "동행", path: "/companion" },
-        { src: "/MenuChat.svg", alt: "채팅", label: "채팅", path: "/chat" },
-        { src: "/MenuUser.svg", alt: "내 정보", label: "내 정보", path: "/staffinfo" },
+        { src: "/NavHome.svg", alt: "홈", label: "홈", path: "/staff" },
+        { src: "/NavCompanion.svg", alt: "동행", label: "동행", path: "/companion" },
+        { src: "/NavChat.svg", alt: "채팅", label: "채팅", path: "/chat" },
+        { src: "/NavUser.svg", alt: "내 정보", label: "내 정보", path: "/staffinfo" },
     ],
 };
 
-export default function Menu({ version }: MenuProps) {
+export default function Nav({ version }: NavProps) {
     const navigate = useNavigate();
-    const menu = menuItems[version];
+    const navMenu = items[version];
 
     const handleClick = (path: string) => {
         navigate(path);
@@ -32,9 +32,9 @@ export default function Menu({ version }: MenuProps) {
 
     return (
         <>
-            <MenuWrapper>
+            <NavWrapper>
                 <Wrapper.FlexBox justifyContent="space-between" alignItems="flex-end">
-                    {menu.map((item, index) => (
+                    {navMenu.map((item, index) => (
                         <Wrapper.FlexBox
                             width="70px"
                             height="50px"
@@ -51,12 +51,12 @@ export default function Menu({ version }: MenuProps) {
                         </Wrapper.FlexBox>
                     ))}
                 </Wrapper.FlexBox>
-            </MenuWrapper>
+            </NavWrapper>
         </>
     );
 }
 
-const MenuWrapper = styled.div`
+const NavWrapper = styled.div`
     position: absolute;
     bottom: 0;
     left: 0;
