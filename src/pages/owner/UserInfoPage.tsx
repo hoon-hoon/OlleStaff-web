caefe310929b344a720d6b563038f6035e729c62
ZUITOPIA <zuitopia.dev@gmail.com>
2025-05-24 19:21:27 +0900

    feat: 내 정보에서 user 이미지, 닉네임 가져와서 보여주는 부분 구현

--- src/pages/owner/UserInfoPage.tsx
+++ src/pages/owner/UserInfoPage.tsx
@@ -1,14 +1,25 @@
 import Header from "@/components/Header";
 import PageWrapper from "@/components/PageWrapper";
 import SettingMenuItem from "@/components/Setting/SettingMenuItem";
+import { useUserStore } from "@/store/useUserStore";
 import { Text } from "@/styles/Text";
 import { Wrapper } from "@/styles/Wrapper";
 
 export default function UserInfoPage() {
+    const user = {
+        image: "/Icon/defaultUser.svg", // 임시 프로필 이미지
+        name: useUserStore(state => state.nickname),
+    };
+
     return (
         <>
             <Header title="내 정보" />
             <PageWrapper hasHeader>
+                <Wrapper.FlexBox direction="column" alignItems="center" gap="12px">
+                    <img src={user.image} alt="프로필이미지" style={{ width: "94px" }} />
+                    <Text.Title3_1>{user.name}</Text.Title3_1>
+                </Wrapper.FlexBox>
+
                 <Wrapper.FlexBox direction="column" gap="12px">
                     <Text.Body1_1>게시글 관리</Text.Body1_1>
                     <div>
