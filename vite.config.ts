51d324a7fa69689cdee7cddec81a06d1a0ce5ee0
hoon-hoon <kc0393@naver.com>
2025-06-17 17:56:39 +0900

    chore: VitePWA 플러그인 설치 및 초기 설정 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,37 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        VitePWA({
+            registerType: "autoUpdate",
+            includeAssets: ["favicon.svg", "apple-touch-icon.png", "images/logo_192.png", "images/logo_512.png"],
+            manifest: {
+                name: "olleStaff",
+                short_name: "olle",
+                start_url: "/",
+                scope: "/",
+                display: "standalone",
+                background_color: "#ffffff",
+                theme_color: "#00C6BE",
+                icons: [
+                    {
+                        src: "images/logo_192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                    },
+                    {
+                        src: "images/logo_512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    },
+                ],
+            },
+        }),
+    ],
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "src"),
